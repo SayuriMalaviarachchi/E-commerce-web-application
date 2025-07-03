d6c2775abc3f3a0956d20f3fa6859d4f6142367a
SayuriMalaviarachchi <sayurisachinthanee11@gmail.com>
2025-07-03 12:06:03 +0530

    Create hero section routing - frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Hero from './components/home/Hero'
 
 function App() {
  
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path = '/register' element = {<Register/>}   />
         <Route path = '/login' element = {<Login/>}   />
+        <Route path = '/hero' element = {<Hero/>} />
       </Routes>
       
     </Router>
