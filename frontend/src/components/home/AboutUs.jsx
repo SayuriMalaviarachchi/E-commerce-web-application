d541dbc6c0d3c76540139bca6a6c8be0c543be99
SayuriMalaviarachchi <sayurisachinthanee11@gmail.com>
2025-07-04 15:47:46 +0530

    Update about us section in home page - frontend

---
+++ frontend/src/components/home/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className='bg-[#bbb8b0] pt-10 pb-10'>
+        <div className='container mx-auto '>
+            <div className="text-center text-[#223531] font-bold sm:text-4xl pb-6">
+              About Us
+            </div>
+            <div className="text-center px-5 text-[#223531] text-[10px] sm:text-[15px] md:text-[20px] ">
+              NestStyle is a modern, fashion-forward web application designed to bring simplicity, elegance, and comfort into your wardrobe. We specialize in offering curated collections for both men and women, focusing on neutral tones, timeless styles, and a minimal aesthetic. Whether you're looking for casual everyday wear or something a bit more refined, NestStyle makes fashion effortless and approachable.At NestStyle, we believe fashion isn’t just about trends—it’s about personal style, confidence, and comfort. We are committed to providing not just products, but inspiration and convenience to help you dress with intention.Whether you're building your capsule wardrobe, exploring minimalist fashion, or simply seeking a new favorite outfit, NestStyle is here to guide you with elegance at every step. Step into a refined world of fashion. Step into NestStyle.
+        </div></div>
+    </div>
+  )
+}
+
+export default AboutUs
